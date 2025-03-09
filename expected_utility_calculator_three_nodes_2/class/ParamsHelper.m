efc0b06d8d3d75cc805a767381f5c66defec4c01
RIKU-SEINO <rikuseino33@gmail.com>
2025-03-09 22:01:30 +0900

    refactor: キャッシュを入 &リファクタリング

--- expected_utility_calculator_three_nodes_2/class/ParamsHelper.m
+++ expected_utility_calculator_three_nodes_2/class/ParamsHelper.m
@@ -1,57 +1,41 @@
 classdef ParamsHelper
   methods (Static)
-    function [w, c, r_0, a, m, p, p_, u, r, q] = getSymbolicParams()
-      % シンボリック変数の定義
-      % syms w c positive
-      % syms r_0 [3 1] positive
-      % syms a [3 1] positive
-      % syms m [3 1] positive
-      % syms p [3 1] positive
-      % syms p_ [3 3] positive
-      % ノード1: 主要都市
-      % ノード2: 副中心都市
-      % ノード3: 田舎
-      w = sym('w', 'positive');
-      c = sym('c', 'positive');
-      r_0 = sym('r_', [3, 1], 'positive');
-      a = sym('a_', [3, 1], 'positive');
-      m = sym('m_', [3, 1], 'positive');
-      p = sym('p_', [3, 1], 'positive');
-      p_ = sym('p_', [3, 3], 'positive');
-      % r_0 = [3000; 2500; 1000];
-      % a = [150; 50; 10];
-      % m = [1; 2; 5];
-      % p = [0.9; 0.7; 0.2];
-      % p_ = [
-      %   0, 1, 0; % 主要中心都市 -> 副中心都市: 1
-      %   1, 0, 0; % 副中心都市 -> 主要中心都市: 1
-      %   0.7, 0.3, 0; % 田舎 -> 主要中心都市: 0.7, 田舎 -> 副中心都市: 0.3
-      % ];
+    function [w, c, r_0, a, p, p_, u, r, q] = getSymbolicParams()
+      persistent cached_params;
+      if ~isempty(cached_params)
+        [w, c, r_0, a, p, p_, u, r, q] = cached_params{:};
+        return;
+      end
+      % w = sym('w', 'positive');
+      w = 2000;
+      % c = sym('c', 'positive');
+      c = 100;
+      % r_0 = sym('r_', [3, 1], 'positive');
+      r_0 = [1000; 2000; 1000];
+      % a = sym('a_', [3, 1], 'positive');
+      a = [50; 100; 50];
+      % p = sym('p_', [3, 1], 'positive');
+      p = [0.1; 0.5; 0.1];
+      % p_ = sym('p_', [3, 3], 'positive');
+      p_ = [0, 1, 0;
+            1, 0, 0;
+            0, 1, 0];
       u = ParamsHelper.calculateTaxiUtilities(c, w);
       r = ParamsHelper.calculatePassengerUtilities(r_0, a);
       q = ParamsHelper.calculateTransitionProbabilityVector(p, p_);
+      cached_params = {w, c, r_0, a, p, p_, u, r, q};
     end
 
-    function u = calculateTaxiUtilities(c,w)
-      nodeNum = 3;
-      u = sym(zeros(nodeNum, nodeNum, nodeNum));
-      for i = 1:nodeNum
-        for j = 1:nodeNum
-          for k = 1:nodeNum
-            u(i, j, k) = -c * (abs(i - j) + abs(j - k)) + w * abs(j - k);
-          end
-        end
-      end
+    function u = calculateTaxiUtilities(c, w)
+      [i, j, k] = ndgrid(1:3, 1:3, 1:3);
+      u = -c * (abs(i - j) + abs(j - k)) + w * abs(j - k);
+      u = sym(u);  % シンボリック変数に変換
     end
-
+  
     function r = calculatePassengerUtilities(r_0, a) 
-      nodeNum = 3;
-      r = sym(zeros(nodeNum, nodeNum));
-      for i = 1:nodeNum
-        for j = 1:nodeNum
-          r(i, j) = r_0(j) - abs(i - j) * a(j);
-        end
-      end
+      [i, j] = ndgrid(1:3, 1:3);
+      r = r_0(i) - a(j);
+      r = sym(r);  % シンボリック変数に変換
     end
 
     function transitionProbabilityVector = calculateTransitionProbabilityVector(p, p_)
